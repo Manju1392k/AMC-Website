62e178b2c022d3b57f5bcada0a4e9284639d4658
Manjunath <sreerammanjunath1392@gmail.com>
2022-05-13 16:00:56 +0530

    AMC

--- AMCApp/forms.py
+++ AMCApp/forms.py
@@ -1,8 +1,26 @@
 
 from django.forms import ModelForm
 from .models import StudentDetails
+from django import forms
+
 
 class StudentForm(ModelForm):
     class Meta:
         model = StudentDetails
-        fields = ['Name', 'Age', 'Father_Name', 'Group', 'Location', 'Mole_One', 'Mole_Two']
+        fields = ['Name', 'Age', 'Father_Name',
+                  'Group', 'Location', 'Mole_One', 'Mole_One']
+
+    Name = forms.CharField(widget=forms.TextInput(
+        attrs={'class': 'form-control'}))
+    Age = forms.CharField(widget=forms.TextInput(
+        attrs={'class': 'form-control'}))
+    Father_Name = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'form-control'}))
+    Group = forms.CharField(widget=forms.TextInput(
+        attrs={'class': 'form-control'}))
+    Location = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'form-control'}))
+    Mole_One = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'form-control'}))
+    Mole_One = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'form-control'}))
