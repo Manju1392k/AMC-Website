1d98774dbf177db38e18bf07ec955c04a4c77d77
Manjunath <sreerammanjunath1392@gmail.com>
2022-05-13 15:45:39 +0530

    AMC

--- AMCApp/models.py
+++ AMCApp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class StudentDetails(models.Model):
+    Name = models.CharField(max_length=100)
+    Age = models.CharField(max_length=2)
+    Father_Name = models.CharField(max_length=100)
+    Group = models.CharField(max_length=4)
+    Location = models.CharField(max_length=150)
+    Mole_One = models.CharField(max_length=70)
+    Mole_Two = models.CharField(max_length=70)
+
+    def __str__(self):
+        return self.Name
