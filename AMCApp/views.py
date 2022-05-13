71df73790ce14db1914c076f71ae729725b773e5
Manjunath <sreerammanjunath1392@gmail.com>
2022-05-13 15:58:14 +0530

    AMC

--- AMCApp/views.py
+++ AMCApp/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from . forms import StudentForm
 
 # Create your views here.
+
+
 def Home(request):
-    return render(request, 'index.html')
+    AdminForm = StudentForm
+    if request.method == 'POST':
+        AdminForm = StudentForm(request.POST)
+        if AdminForm.is_valid():
+            AdminForm.save()
+    return render(request, 'index.html', {'AdminForm': AdminForm})
